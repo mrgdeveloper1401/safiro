5011f8088edf06ec48acba176efff703356927da
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2026-05-02 18:09:23 +0330

    edit asgi

--- base/asgi.py
+++ base/asgi.py
@@ -1,27 +1,21 @@
-# import os
-#
-# from django.core.asgi import get_asgi_application
-#
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
-#
-# django_application = get_asgi_application()
-#
-# async def application(scope, receive, send):
-#     if scope['type'] == 'lifespan':
-#         while True:
-#             message = await receive()
-#             if message['type'] == 'lifespan.startup':
-#                 # print("✅ Django ASGI startup event received")
-#                 await send({'type': 'lifespan.startup.complete'})
-#             elif message['type'] == 'lifespan.shutdown':
-#                 # print("❌ Django ASGI shutdown event received")
-#                 await send({'type': 'lifespan.shutdown.complete'})
-#                 return
-#     else:
-#         await django_application(scope, receive, send)
-
 import os
+
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
-application = get_asgi_application()
+
+django_application = get_asgi_application()
+
+async def application(scope, receive, send):
+    if scope['type'] == 'lifespan':
+        while True:
+            message = await receive()
+            if message['type'] == 'lifespan.startup':
+                # print("✅ Django ASGI startup event received")
+                await send({'type': 'lifespan.startup.complete'})
+            elif message['type'] == 'lifespan.shutdown':
+                # print("❌ Django ASGI shutdown event received")
+                await send({'type': 'lifespan.shutdown.complete'})
+                return
+    else:
+        await django_application(scope, receive, send)
