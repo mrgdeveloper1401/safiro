f086d679cfda728cceb9d8dc611703bc2ba55a82
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2026-05-02 20:00:17 +0330

    comment get_clickhouse_client

--- apps/shop_app/tasks.py
+++ apps/shop_app/tasks.py
@@ -1,16 +1,16 @@
 from celery import shared_task
 
-from base.dj_clickhouse import get_clickhouse_client
-
-
-@shared_task(bind=True, max_retries=2, default_retry_delay=5, queue="shop_event")
-def task_track_event(self, user_id, product_id, event_type, ts):
-    try:
-        client = get_clickhouse_client()
-        client.insert(
-            "user_event",
-            [[user_id, product_id, event_type, ts]],
-            column_names=['user_id', 'product_id', 'event_type', 'created_at']
-        )
-    except Exception as e:
-        raise self.retry(exc=e)
+# from base.dj_clickhouse import get_clickhouse_client
+#
+#
+# @shared_task(bind=True, max_retries=2, default_retry_delay=5, queue="shop_event")
+# def task_track_event(self, user_id, product_id, event_type, ts):
+#     try:
+#         client = get_clickhouse_client()
+#         client.insert(
+#             "user_event",
+#             [[user_id, product_id, event_type, ts]],
+#             column_names=['user_id', 'product_id', 'event_type', 'created_at']
+#         )
+#     except Exception as e:
+#         raise self.retry(exc=e)
